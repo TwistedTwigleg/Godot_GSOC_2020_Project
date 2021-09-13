09e278b9484bc57d21b56b1680de7b3675f62031
TwistedTwigleg <beard.noah@gmail.com>
2021-09-13 18:02:44 -0400

    Added IK modifications to all IK test scenes. Slightly modified code to get it working withe latest changes in GDSCript

--- skeleton_modification/scenes/skeleton_test_09_character_fps/Scene_09_SimplePlayer.gd
+++ skeleton_modification/scenes/skeleton_test_09_character_fps/Scene_09_SimplePlayer.gd
@@ -4,7 +4,7 @@ var head_node:Node3D;
 var body_node:Node3D;
 var MOUSE_SENSITIVITY:float = 0.05
 
-var bullet_scene:PackedScene = preload("res://Scenes/Skeleton_Test_09/Test_09_Bullet.tscn");
+var bullet_scene:PackedScene = preload("res://skeleton_modification/scenes/skeleton_test_09_character_fps/Test_09_Bullet.tscn");
 var gun_end:Node3D;
 var fire_timer:float = 0;
 const FIRE_WAIT_TIME:float = 0.2;
@@ -15,6 +15,9 @@ var gun_shake_strength = 0.0;
 
 var gun_animation_player:AnimationPlayer;
 var current_animation = "Idle";
+
+var random_number_generator:RandomNumberGenerator = RandomNumberGenerator.new();
+
 
 func _ready():
 	head_node = $Body/Head as Node3D
@@ -39,15 +42,16 @@ func _process(delta:float):
 	
 	if Input.is_action_pressed("fire"):
 		if (fire_timer <= 0):
-			var clone = bullet_scene.instance();
+			var clone = bullet_scene.instantiate();
 			clone.global_transform = gun_end.global_transform;
 			clone.scale = Vector3(1, 1, 1);
-			clone.linear_velocity = gun_end.global_transform.basis.get_rotation_quat().xform(Vector3(0, 0, 32));
+			#clone.linear_velocity = gun_end.global_transform.basis.get_rotation_quat().xform(Vector3(0, 0, 32));
+			clone.linear_velocity = gun_end.global_transform.basis.get_rotation_quaternion() * Vector3(0, 0, 32);
 			get_parent().add_child(clone);
 			
-			gun_shake_strength = 1;
-			gun_shake_rotation.x = deg2rad(rand_range(-60, 60));
-			gun_shake_rotation.y = deg2rad(rand_range(-60, 60));
+			gun_shake_strength = 0.25;
+			gun_shake_rotation.x = deg2rad(random_number_generator.randf_range(-60, 60));
+			gun_shake_rotation.y = deg2rad(random_number_generator.randf_range(-60, 60));
 			fire_timer = FIRE_WAIT_TIME;
 	fire_timer -= delta;
 	
@@ -63,7 +67,10 @@ func _process(delta:float):
 				current_animation = "Idle";
 	
 	#gun.rotation_degrees = gun_shake_rotation * gun_shake_strength;
-	gun.rotation_degrees = lerp(gun.rotation_degrees, gun_shake_rotation * gun_shake_strength, delta * 20);
+	#gun.rotation = lerp(gun.rotation, gun_shake_rotation * gun_shake_strength, delta * 20);
+	#gun.rotation_degrees = lerp(gun.rotation_degrees, gun_shake_rotation * gun_shake_strength, delta * 20);
+	gun.rotation = gun.rotation.lerp(gun_shake_rotation * gun_shake_strength, delta * 20.0);
+	
 	if (gun_shake_strength > 0):
 		gun_shake_strength = clamp(gun_shake_strength - (delta * 6), 0, 100);
 
@@ -73,6 +80,6 @@ func _input(event):
 		head_node.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY))
 		body_node.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1))
 
-		var camera_rot = head_node.rotation_degrees
-		camera_rot.x = clamp(camera_rot.x, -70, 70)
-		head_node.rotation_degrees = camera_rot
+		var camera_rot = head_node.rotation
+		camera_rot.x = clamp(camera_rot.x, deg2rad(-70), deg2rad(70))
+		head_node.rotation = camera_rot
